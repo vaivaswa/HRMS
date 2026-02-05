e641e87fc71450d713d36897b2d19f61fbcd51d2
viavaswat <vaivaswatpribextech@gmail.com>
2026-02-05 19:21:02 +0530

    Add whitenoise for static files

--- employees/models.py
+++ employees/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Employee(models.Model):
-    emp_id = models.CharField(max_length=10, unique=True)
+    emp_id = models.CharField(max_length=10, unique=True, blank=True, null=True)
     Full_name = models.CharField(max_length=100)
     email = models.EmailField()
     department = models.CharField(max_length=50)
